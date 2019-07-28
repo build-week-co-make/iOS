9b64c0b3d76d3f1f147c8c034899dcadfefa2737
Luqmaan Khan <luq.j.khan@gmail.com>
2019-07-28 16:15:54 -0500

    Welcome view location view tab bar and icons added

---
+++ Co-Make/Co-Make/AllowLocationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AllowLocationViewController.swift
+//  Co-Make
+//
+//  Created by Luqmaan Khan on 7/28/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class AllowLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func allowLocationTapped(_ sender: UIButton) {
+    let sb = UIStoryboard(name: "Main", bundle: nil)
+    let mainView = sb.instantiateViewController(withIdentifier: "MainView")
+        present(mainView, animated: true, completion: nil)
+    
+    }
+}
