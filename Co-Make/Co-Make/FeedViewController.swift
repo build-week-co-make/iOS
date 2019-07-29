aa97869b7b270607e5731994d13a58d44be8d641
Luqmaan Khan <luq.j.khan@gmail.com>
2019-07-29 15:24:54 -0500

    Cell view added

--- Co-Make/Co-Make/FeedViewController.swift
+++ Co-Make/Co-Make/FeedViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    @IBOutlet var userImage: UIImageView!
+    
+    @IBOutlet var userActualName: UILabel!
+    
+    @IBOutlet var userAddress: UILabel!
+    
+    @IBOutlet var issuesTableView: UITableView!
+    
+    @IBOutlet var searchIssues: UISearchBar!
+    
+    let sampleData = ["sampledata"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchIssues.cornerRadius = 20
+        userImage.image = UIImage(named: "sign-in-4")
+        userImage.cornerRadius = 23
         
     }
     
@@ -27,4 +42,30 @@ class FeedViewController: UIViewController {
     }
     */
 
+}
+extension FeedViewController: UITableViewDelegate{
+    
+}
+extension FeedViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard  let cell = issuesTableView.dequeueReusableCell(withIdentifier: "IssueCell", for: indexPath) as? IssueTableViewCell else {return UITableViewCell()}
+        
+        let selectedIssue = sampleData[indexPath.row]
+        
+        cell.firstFIlterType.text = selectedIssue
+        cell.issueCreator.text = selectedIssue
+        cell.issueTitle.text = selectedIssue
+        cell.numberOfUpVotes.text = "5 Votes"
+        cell.secondFilterType.text = selectedIssue
+        cell.issueImage.cornerRadius = 15
+        cell.issueImage.image = UIImage(named: "sign-in-4")
+      
+        return cell
+    }
+    
+    
 }
