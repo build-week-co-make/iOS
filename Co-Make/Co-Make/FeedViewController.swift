e7777301c6107e14c7ae085d7bb2c93261e3a1e9
Luqmaan Khan <luq.j.khan@gmail.com>
2019-07-28 19:24:51 -0500

    Feed view complete

---
+++ Co-Make/Co-Make/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Co-Make
+//
+//  Created by Luqmaan Khan on 7/28/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
