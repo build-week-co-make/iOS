2259dc21cab8e754952b0e582d64260db337483a
shillwil <alex.shillingford@icloud.com>
2019-07-31 13:52:10 -0600

    Update the CreateIssueVC with the red background, addPhoto button, zip code text view, and postIssue button

--- Co-Make/Co-Make/ViewControllers/CreateIssueViewController.swift
+++ Co-Make/Co-Make/ViewControllers/CreateIssueViewController.swift
@@ -9,31 +9,43 @@
 import UIKit
 
 class CreateIssueViewController: UIViewController {
-    
+    //MARK: - IBOutlets and Properties
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var issueImageView: UIImageView!
-    @IBOutlet weak var takePhotoButton: UIButton!
-    @IBOutlet weak var addPhotoButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var zipCodeTextField: UITextField!
     
-    
+//    let apiController: ApiController? 
     let pickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        takePhotoButton.layer.cornerRadius = 5.0
-        addPhotoButton.layer.cornerRadius = 5.0
+        titleTextField.layer.cornerRadius = 5.0
         pickerController.sourceType = UIImagePickerController.SourceType.camera
         pickerController.delegate = self
     }
     
-
-    @IBAction func takePhotoButtonTapped(_ sender: UIButton) {
+    //MARK: - IBActions and Methods
+    @IBAction func takePhotoButtonTapped(_ sender: UIImageView) {
         present(pickerController, animated: true, completion: nil)
     }
     
+    @IBAction func postIssueButtonTapped(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
 }
 
+//MARK: - Extensions
 extension CreateIssueViewController: UIImagePickerControllerDelegate {
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
